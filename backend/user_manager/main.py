52e89152ed2437076970720a602b03cebf5b055e
serk1llz <ilgam.yakhin@gmail.com>
2024-06-17 18:21:17 +0300

    fixed_isort

--- backend/user_manager/main.py
+++ backend/user_manager/main.py
@@ -5,7 +5,6 @@ from fastapi import Depends, FastAPI
 from fastapi_users import FastAPIUsers
 from schemas import UserCreate, UserRead
 from starlette.middleware.cors import CORSMiddleware
-
 from user_manager import get_user_manager
 
 app = FastAPI()
