90f92a52f07987ea95fe3679c789d85dc7046414
AAB111 <dikaprio-1986@mail.ru>
2024-06-17 08:46:54 +0300

    added unit_tests, fixed dockerfiles

--- backend/task_manager/src/api/router.py
+++ backend/task_manager/src/api/router.py
@@ -5,7 +5,7 @@ from src.api.schemas import TaskPOST, TaskDelete, TaskPatch, Task
 from src.services.services import TaskService
 from src.api.utils import Paginator
 from src.db.models import User
-
+from src.db.database import async_session_maker
 from src.api.auth import auth_backend
 from src.api.user_manager import get_user_manager
 
@@ -22,10 +22,11 @@ current_user = fastapi_users.current_user()
 @router.get('/count')
 async def count_completed(is_completed: bool = None, user: User = Depends(current_user)):
     try:
-        result = await TaskService(user.id).get_count_by_completed(is_completed)
-        if result['success']:
-            return {'count': result['count']}
-        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content='Not valid data')
+        async with async_session_maker() as session:
+            result = await TaskService(user.id, session).get_count_by_completed(is_completed)
+            if result['success']:
+                return {'count': result['count']}
+            return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content='Not valid data')
     except Exception as e:
         print(e)
         return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
@@ -35,10 +36,11 @@ async def count_completed(is_completed: bool = None, user: User = Depends(curren
 @router.delete('/delete/{task_id}')
 async def delete_task_for_user(task_id: int, user: User = Depends(current_user)):
     try:
-        result = await TaskService(user.id).delete_task_for_user(task_id)
-        if result['success']:
-            return JSONResponse(status_code=status.HTTP_200_OK, content='Success')
-        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content='Not valid data')
+        async with async_session_maker() as session:
+            result = await TaskService(user.id, session).delete_task_for_user(task_id)
+            if result['success']:
+                return JSONResponse(status_code=status.HTTP_200_OK, content='Success')
+            return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content='Not valid data')
     except Exception as e:
         print(e)
         return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
@@ -48,10 +50,11 @@ async def delete_task_for_user(task_id: int, user: User = Depends(current_user))
 @router.get('/{task_id}')
 async def get_task_by_id(task_id: int, user: User = Depends(current_user)):
     try:
-        result = await TaskService(user.id).get_task_by_id(task_id)
-        if (result['success']) & (result['data'] is not None):
-            return Task.model_validate(result['data'], from_attributes=True)
-        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content='Not valid data')
+        async with async_session_maker() as session:
+            result = await TaskService(user.id, session).get_task_by_id(task_id)
+            if (result['success']) & (result['data'] is not None):
+                return Task.model_validate(result['data'], from_attributes=True)
+            return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content='Not valid data')
     except Exception as e:
         print(e)
         return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
@@ -62,14 +65,15 @@ async def get_task_by_id(task_id: int, user: User = Depends(current_user)):
 async def get_all_tasks(is_completed: bool = None, user: User = Depends(current_user),
                         paginator: Paginator = Depends()):
     try:
-        result = await TaskService(user.id).get_all_tasks(is_completed, paginator)
-        if (result['success']) & (result['data'] is not None):
-            output_tasks = [
-                Task.model_validate(task, from_attributes=True)
-                for task in result['data']
-            ]
-            return output_tasks
-        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content='Not valid data')
+        async with async_session_maker() as session:
+            result = await TaskService(user.id, session).get_all_tasks(is_completed, paginator)
+            if (result['success']) & (result['data'] is not None):
+                output_tasks = [
+                    Task.model_validate(task, from_attributes=True)
+                    for task in result['data']
+                ]
+                return output_tasks
+            return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content='Not valid data')
     except Exception as e:
         print(e)
         return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
@@ -79,10 +83,11 @@ async def get_all_tasks(is_completed: bool = None, user: User = Depends(current_
 @router.post('/add')
 async def add_task_for_user(params: TaskPOST, user: User = Depends(current_user)):
     try:
-        result = await TaskService(user.id).add_task_for_user(params.title, params.target_date)
-        if (result['success']):
-            return JSONResponse(status_code=status.HTTP_200_OK, content='Success')
-        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content='Not valid data')
+        async with async_session_maker() as session:
+            result = await TaskService(user.id, session).add_task_for_user(params.title, params.target_date)
+            if result['success'] is True:
+                return JSONResponse(status_code=status.HTTP_200_OK, content='Success')
+            return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content='Not valid data')
     except Exception as e:
         print(e)
         return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
@@ -93,11 +98,11 @@ async def add_task_for_user(params: TaskPOST, user: User = Depends(current_user)
 async def update_task_for_user(params: TaskPatch,
                                user: User = Depends(current_user)):
     try:
-        print(params.dict())
-        result = await TaskService(user.id).update_task_for_user(**params.dict())
-        if result['success']:
-            return JSONResponse(status_code=status.HTTP_200_OK, content='Success')
-        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content='Not valid data')
+        async with async_session_maker() as session:
+            result = await TaskService(user.id, session).update_task_for_user(**params.dict())
+            if result['success']:
+                return JSONResponse(status_code=status.HTTP_200_OK, content='Success')
+            return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content='Not valid data')
     except Exception as e:
         print(e)
         return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
---
+++ backend/user_manager/tests/test.py
@@ -0,0 +1,37 @@
+import pytest
+from httpx import AsyncClient
+
+async def test_register(ac: AsyncClient):
+    response = await ac.post('/auth/register', json={
+        "email": "test220@mail.ru",
+        "password": "test",
+    })
+
+    assert response.status_code == 201
+
+
+async def test_login(ac: AsyncClient):
+    response = await ac.post('/auth/jwt/login', data={
+        "username": "test220@mail.ru",
+        "password": "test",
+    })
+
+    assert response.status_code == 204
+
+
+async def test_not_valid_data_register(ac: AsyncClient):
+    response = await ac.post('/auth/register', json={
+        "email": "test10",
+        "password": "test",
+    })
+
+    assert response.status_code == 422
+
+
+async def test_not_valid_data_login(ac: AsyncClient):
+    response = await ac.post('/auth/jwt/login', data={
+        "username": "test10",
+        "password": "test",
+    })
+
+    assert response.status_code == 400
