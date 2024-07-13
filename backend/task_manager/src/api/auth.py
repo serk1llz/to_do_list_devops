278093c23416ddae8f38538e537bf37ddfaab572
AAB111 <aliosha.andrianov.2002@mail.ru>
2024-07-13 17:29:30 +0300

    fixed_secure

--- backend/task_manager/src/api/auth.py
+++ backend/task_manager/src/api/auth.py
@@ -5,7 +5,7 @@ from fastapi_users.authentication import (
     JWTStrategy,
 )
 
-cookie_transport = CookieTransport(cookie_name="token", cookie_max_age=3600, cookie_secure=False)
+cookie_transport = CookieTransport(cookie_name="token", cookie_max_age=3600)
 
 
 SECRET = settings.SECRET
