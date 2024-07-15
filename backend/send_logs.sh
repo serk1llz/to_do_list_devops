6cda73593bb28b4f87bc43713429cbf713546330
serk1llz <136832339+serk1llz@users.noreply.github.com>
2024-07-15 07:31:08 +0300

    Update send_logs.sh

--- backend/send_logs.sh
+++ backend/send_logs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl -F chat_id=${TELEGRAM_CHAT_ID} -F document=@task_manager.txt https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendDocument
-curl -F chat_id=${TELEGRAM_CHAT_ID} -F document=@user_manager.txt https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendDocument
+curl -F chat_id=-1002177314625 -F document=@task_manager.txt https://api.telegram.org/bot7201827704:AAHiKRMZe4iQM0wUmNnPXdygqez6zUSNJbI/sendDocument
+curl -F chat_id=-1002177314625 -F document=@user_manager.txt https://api.telegram.org/bot7201827704:AAHiKRMZe4iQM0wUmNnPXdygqez6zUSNJbI/sendDocument
