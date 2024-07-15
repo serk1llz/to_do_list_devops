072df1563806dea98746a47ef27e47884b5cbda3
serk1llz <136832339+serk1llz@users.noreply.github.com>
2024-07-15 06:35:21 +0300

    Update check_test.sh

--- backend/check_test.sh
+++ backend/check_test.sh
@@ -1,32 +1,24 @@
-#!/bin/bash
-
-# Функция для проверки логов контейнера и записи их в файл
+в скрипте backend/check_test.sh
 check_logs() {
   local container_name=$1
-  local log_file="${container_name}_logs.txt"
+  output=$(docker logs --tail 10 "$container_name" | grep failed)
 
-  docker logs --tail 10 "$container_name" > "$log_file"
-  
-  # Проверка наличия ключевых слов в логах
-  if grep -q "failed" "$log_file"; then
-    echo "Failures found in logs of $container_name. See $log_file for details."
-    echo "$log_file"
-  else
+  if [[ $? -ne 0 ]]; then
     echo "No failures found in logs of $container_name."
-    rm "$log_file"
-    echo ""
+  else
+    echo "Failures found in logs of $container_name:"
+    echo "$output"
+    return 1
   fi
 }
 
-# Проверка логов каждого контейнера
 check_logs task_manager_container_test
-task_manager_logs=$?
+task_manager_status=$?
 
 check_logs user_manager_container_test
-user_manager_logs=$?
+user_manager_status=$?
 
-# Возвращаем соответствующий код выхода
-if [[ $task_manager_logs -ne 0 || $user_manager_logs -ne 0 ]]; then
+if [[ $task_manager_status -ne 0 || $user_manager_status -ne 0 ]]; then
   exit 1
 else
   exit 0
