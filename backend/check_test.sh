f2d06324351c6b19d9dfdf127778f8a2a95feded
serk1llz <136832339+serk1llz@users.noreply.github.com>
2024-07-15 06:24:01 +0300

    Update check_test.sh

--- backend/check_test.sh
+++ backend/check_test.sh
@@ -1,25 +1,34 @@
+#!/bin/bash
+
+# Функция для проверки логов контейнера и записи их в файл
 check_logs() {
   local container_name=$1
-  output=$(docker logs --tail 10 "$container_name" | grep failed)
+  local log_file="${container_name}_logs.txt"
 
-  if [[ $? -ne 0 ]]; then
-    echo "No failures found in logs of $container_name."
+  docker logs --tail 10 "$container_name" > "$log_file"
+  
+  # Проверка наличия ключевых слов в логах
+  if grep -q "failed" "$log_file"; then
+    echo "Failures found in logs of $container_name. See $log_file for details."
+    echo "$log_file"
   else
-    echo "Failures found in logs of $container_name:"
-    echo "$output"
-    echo "::set-output name=${container_name}_logs::$output"
-    return 1
+    echo "No failures found in logs of $container_name."
+    rm "$log_file"
+    echo ""
   fi
 }
 
+# Проверка логов каждого контейнера
 check_logs task_manager_container_test
-task_manager_status=$?
+task_manager_logs=$?
 
 check_logs user_manager_container_test
-user_manager_status=$?
+user_manager_logs=$?
 
-if [[ $task_manager_status -ne 0 || $user_manager_status -ne 0 ]]; then
+# Возвращаем соответствующий код выхода
+if [[ $task_manager_logs -ne 0 || $user_manager_logs -ne 0 ]]; then
   exit 1
 else
   exit 0
 fi
+
